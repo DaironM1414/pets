3a1fac22c8682a3ec885085c94803976d6bc6289
DaironM1414 <mdairon553@gmail.com>
2024-05-31 12:44:22 -0500

    uploading src

--- src/list_users.php
+++ src/list_users.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <title>Document</title>
 </head>
 <body>
@@ -13,25 +13,36 @@
            LIST USERS 
         </h1>
     </center>
-    <table class="table table-striped">
-        <tr>
-            <th>Fullname</th>
-            <th>Email</th>
-            <th>Status</th>
-            <th>Foto</th>
-            <th>...</th>
-        </tr>
-        <tr>
-            <td>Samir Alih</td>
-            <td>example@example.com</td>
-            <td>Active</td>
-            <td>
-                <img src="icons/users.png" width="32">
-            </td>
-            <td>
-                <a href="#"><img src="icons/edit.png" width="32"></a>
-                <a href="#"><img src="icons/delete.png" width="32" height="32"></a>
-            </td>
-        </tr>
+    <table id="user-table" class="table table-striped">
+        <thead>
+            <tr>
+                <th>Fullname</th>
+                <th>Email</th>
+                <th>Status</th>
+                <th>Foto</th>
+                <th>...</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Samir Alih</td>
+                <td>example@example.com</td>
+                <td>Active</td>
+                <td>
+                    <img src="icons/users.png" width="32">
+                </td>
+                <td>
+                    <button class="btn btn-danger delete-btn" onclick="deleteRow(this)">Delete</button>
+                </td>
+            </tr>
+        </tbody>
     </table>
+
+    <script>
+        function deleteRow(button) {
+            var row = button.parentNode.parentNode;
+            row.parentNode.removeChild(row);
+        }
+    </script>
 </body>
+</html>
